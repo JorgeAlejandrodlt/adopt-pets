ce11d80382c0afcd5e722710db70379cdfb576cc
Jorge de la Torre <jorge_dlt1992@hotmail.com>
2019-12-11 16:44:50 -0500

    Add files via upload

---
+++ src/data/cats.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    name: "Fish",
+    breed: "tuxedo",
+    species: "cat",
+    gender: "male",
+    age: 20,
+    color: "black/white",
+    weight: 13,
+    location: "fourside",
+    notes: "Sweet kitty. He loves getting his belly rubbed."
+  },
+  {
+    name: "Henry",
+    breed: "tabby",
+    species: "cat",
+    gender: "male",
+    age: 20,
+    color: "orangewhite",
+    weight: 17,
+    location: "thread",
+    notes: "Super friendly"
+  },
+  {
+    name: "Roger",
+    breed: "tabby",
+    species: "cat",
+    gender: "male",
+    age: 20,
+    color: "gray",
+    weight: 15,
+    location: "thread",
+    notes: "Sweet kitty. He loves getting his belly rubbed."
+  },
+  {
+    name: "Kitkat",
+    breed: "bombay",
+    species: "cat",
+    gender: "female",
+    age: 9,
+    color: "black",
+    weight: 9,
+    location: "thread",
+    notes: "Super friendly"
+  }
+];
